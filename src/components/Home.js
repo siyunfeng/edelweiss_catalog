9507e3c20cd8fdb73fe2025805df2f58130635e6
Siyun Feng <siyunf21@gmail.com>
2023-08-14 14:55:44 -0400

    set up basic catalog info on Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,18 @@
+import data from '../data.json';
+
 const Home = () => {
-  return <div></div>;
+  const { name, imageUri, orgName } = data;
+  return (
+    <div className='catalogs-container'>
+      <div className='each-catalog-container'>
+        <img src={imageUri} alt='catalog cover' />
+        <div className='catalog-info'>
+          <div>{name}</div>
+          <div>{orgName}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
