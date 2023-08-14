3d1e3cc6056ac5024aaefef32592e0b0d88dc0b1
Siyun Feng <siyunf21@gmail.com>
2023-08-14 15:19:49 -0400

    added cover image, product name and author to AllProducts component

--- src/components/AllProducts.js
+++ src/components/AllProducts.js
@@ -1,5 +1,40 @@
+import data from '../data.json';
+import { Link } from 'react-router-dom';
+import defaultCoverImage from '../images/imageNotAvailable.png';
+
 const AllProducts = () => {
-  return <div></div>;
+  const { products } = data;
+  return (
+    <div className='all-products-container'>
+      {products
+        ? products.map((product) => {
+            let coverImage = defaultCoverImage;
+            if (product.images && product.images.length) {
+              let jacketCover = product.images.filter((cover) =>
+                cover.uri.includes('jacket_covers')
+              );
+              if (jacketCover[0]) {
+                coverImage = jacketCover[0].uri;
+              }
+            }
+            return (
+              <div className='each-product-container'>
+                <img
+                  src={coverImage || defaultCoverImage}
+                  alt='product cover'
+                />
+                <div className='catalog-info'>
+                  <Link to={`/products/${product.sku}`}>
+                    <div>{product.fullName || 'Name not available'}</div>
+                  </Link>
+                  <div>Author: {product.author || 'not available'}</div>
+                </div>
+              </div>
+            );
+          })
+        : 'Product not found'}
+    </div>
+  );
 };
 
 export default AllProducts;
