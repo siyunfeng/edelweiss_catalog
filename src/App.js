40ec9950cc10025325952f6f4e4d5389caeb271f
Siyun Feng <siyunf21@gmail.com>
2023-08-14 14:41:56 -0400

    set up react routers, created Home, AllProducts, SingleProduct components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+import AllProducts from './components/AllProducts';
+import SingleProducts from './components/SingleProducts';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <Link to='/'>
+            <img
+              src='https://static.wixstatic.com/media/9067a8_f4d97003e1914861b30db64bbf03bda1~mv2.png/v1/fill/w_394,h_57,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Vicki%20-%204%20rays%20tweaked.png'
+              className='App-logo'
+              alt='logo'
+            />
+          </Link>
+        </header>
+      </div>
+      <Routes>
+        <Route exact path='/' Component={Home} />
+        <Route exact path='/catalogs/:catalogId' Component={AllProducts} />
+        <Route exact path='/products/:productId' Component={SingleProducts} />
+      </Routes>
+    </Router>
   );
 }
 
