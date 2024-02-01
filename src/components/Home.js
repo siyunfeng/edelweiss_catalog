e45e9af9aa13ad59a553503447afa1d8a087cc6c
Siyun Feng <siyunf21@gmail.com>
2024-01-31 23:18:29 -0500

    fixed invalid image url error

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import data from '../data.json';
 import { Link } from 'react-router-dom';
+import defaultCoverImage from '../images/imageNotAvailable.png';
 
 const Home = () => {
   const { catalogID, name, imageUri, orgName } = data;
@@ -7,9 +8,16 @@ const Home = () => {
     <div className='catalogs-container'>
       <Link to={`/catalogs/${catalogID}`}>
         <div className='each-catalog-container'>
-          <img src={imageUri} alt='catalog cover' />
+          <img
+            src={imageUri}
+            alt='catalog cover'
+            onError={(event) => {
+              event.target.onerror = null;
+              event.target.src = defaultCoverImage;
+            }}
+          />
           <div className='catalog-info'>
-            <div>{name}</div>
+            <div className='title'>{name}</div>
             <div>{orgName}</div>
           </div>
         </div>
