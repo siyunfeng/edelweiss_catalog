d2681dfe70f42b0fb36f83ad76a8f5cad7997d41
Siyun Feng <siyunf21@gmail.com>
2024-01-31 23:20:25 -0500

    updated website logo and styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './components/Home';
 import AllProducts from './components/AllProducts';
 import SingleProduct from './components/SingleProduct';
+import logoText from './images/edelweiss-logo-text.png';
+import logoIcon from './images/edelweiss-logo-icon.png';
 
 function App() {
   return (
@@ -10,11 +12,10 @@ function App() {
       <div className='App'>
         <header className='App-header'>
           <Link to='/'>
-            <img
-              src='https://static.wixstatic.com/media/9067a8_f4d97003e1914861b30db64bbf03bda1~mv2.png/v1/fill/w_394,h_57,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Vicki%20-%204%20rays%20tweaked.png'
-              className='App-logo'
-              alt='logo'
-            />
+            <div className='App-logo-container'>
+              <img src={logoText} className='App-logo' alt='logo-icon' />
+              <img src={logoIcon} className='App-logo' alt='logo-text' />
+            </div>
           </Link>
         </header>
       </div>
